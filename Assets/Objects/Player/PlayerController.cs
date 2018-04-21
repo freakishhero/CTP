52c8e076ffe6c73174e79787f107758b8ac948fc
Mitchell Height <Mitchell2.Height@live.uwe.ac.uk>
2018-04-21 20:00:29 +0100

    cleaned up player controller. No longer loops unecessarily and calculation time is thus decreased.

--- Assets/Objects/Player/PlayerController.cs
+++ Assets/Objects/Player/PlayerController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     // Use this for initialization
     void Start()
@@ -27,14 +28,14 @@ public class PlayerController : MonoBehaviour {
             {
                 SelectTile();
             }
-        } 
+        }
     }
 
     void EndTurn()
     {
         if (GameData.PlayersTurn < 4)
         {
-            if(GameData.PlayersTurn > 0 && GameData.PlayersTurn < 4)
+            if (GameData.PlayersTurn > 0 && GameData.PlayersTurn < 4)
                 Game.getCPUs()[GameData.PlayersTurn - 1].GetComponent<AI>().setTurnState(false);
 
             GameData.PlayersTurn++;
@@ -58,66 +59,63 @@ public class PlayerController : MonoBehaviour {
         //Debug.DrawLine(transform.position, target);
         //Debug.Log(target);
 
-        if (hit.collider != null)
-        {
-            if (hit.collider.gameObject.tag == "Sheep1")
-            {
-                TileData data = hit.collider.gameObject.GetComponent<TileData>();
+        if (hit.collider == null)
+            return false;
 
-                //LOGIC FOR SPLITTING STACK
-                if (data.StackSize > 1)
+        if (hit.collider.gameObject.tag != "Sheep1")
+            return false;
+
+        TileData data = hit.collider.gameObject.GetComponent<TileData>();
+
+        //LOGIC FOR SPLITTING STACK
+        if (data.StackSize > 1)
+        {
+            bool moved = false;
+            ushort lastIDqueried = 0;
+            int queriedTiles = 0;
+            foreach (GameObject tile in Game.getBoard())
+            {
+                if (!moved)
                 {
-                    bool moved = false;
-                    ushort lastIDqueried = 0;
-                    int queriedTiles = 0;
-                    foreach (GameObject tile in Game.getBoard())
-                    { 
-                        if (!moved)
+                    if (tile != hit.collider.gameObject)
+                    {
+                        TileData d = tile.GetComponent<TileData>();
+                        Debug.Log("Tile position: " + tile.transform.position);
+                        Debug.Log("checker position: " + data.TileChecker.transform.position);
+                        if (data.TileChecker.transform.position == tile.transform.position && tile.gameObject.tag == "Tile")
                         {
-                            if (tile != hit.collider.gameObject)
+                            queriedTiles++;
+                            lastIDqueried = d.ID;
+                            Debug.Log("On an empty tile. Tile ID is:" + lastIDqueried);
+                        }
+                        else
+                        {
+                            moved = true;
+                            if (queriedTiles > 0)
                             {
-                                TileData d = tile.GetComponent<TileData>();
-                                Debug.Log("Tile position: " + tile.transform.position);
-                                Debug.Log("checker position: " + data.TileChecker.transform.position);
-                                if (data.TileChecker.transform.position == tile.transform.position && tile.gameObject.tag == "Tile")
-                                {
-                                    queriedTiles++;
-                                    lastIDqueried = d.ID;
-                                    Debug.Log("On an empty tile. Tile ID is:" + lastIDqueried);
-                                }
-                                else
-                                {
-                                    moved = true;
-                                    if (queriedTiles > 0)
-                                    {
-                                        data.StackSize /= 2;
-                                        Game.getBoard()[lastIDqueried].GetComponent<TileData>().Owner = 1;
-                                        Game.getBoard()[lastIDqueried].GetComponent<TileData>().StackSize = data.StackSize;
-                                        Debug.Log("Hit an occupied tile or the end of the game board. Cannot progress. Last unoccupied tile was " + lastIDqueried);
-                                    }
-                                    else
-                                    {
-                                        Debug.Log("This stack cannot move in that direction.");
-                                    }
-                                    
-                                }
+                                data.StackSize /= 2;
+                                Game.getBoard()[lastIDqueried].GetComponent<TileData>().Owner = 1;
+                                Game.getBoard()[lastIDqueried].GetComponent<TileData>().StackSize = data.StackSize;
+                                Debug.Log("Hit an occupied tile or the end of the game board. Cannot progress. Last unoccupied tile was " + lastIDqueried);
                             }
-                            data.TileChecker.transform.position += Vector3.right * 2;
+                            else
+                            {
+                                Debug.Log("This stack cannot move in that direction.");
+                            }
+
                         }
                     }
-                    moved = false;
-                    queriedTiles = 0;
-                    data.TileChecker.transform.position = hit.collider.transform.position;
-                    lastIDqueried = 0;
+                    data.TileChecker.transform.position += Vector3.right * 2;
                 }
-                //Debug.Log("sheep stack hit");
-                
             }
+            moved = false;
+            queriedTiles = 0;
+            data.TileChecker.transform.position = hit.collider.transform.position;
+            lastIDqueried = 0;
+
             return true;
         }
-        else
-        {
-            return false;
-        }
+        return false;
     }
+
 }
